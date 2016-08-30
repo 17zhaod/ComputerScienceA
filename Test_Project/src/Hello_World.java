3304f0c36a194f797196be0f4733e671feb52334
daranzhao <17zhaod@smtexas.org>
2016-08-30 13:48:52 -0500

    Removed comment

--- Test_Project/src/Hello_World.java
+++ Test_Project/src/Hello_World.java
@@ -2,7 +2,6 @@
 public class Hello_World {
 
 	    public static void main(String[] args) {
-	        // Prints "Hello, World" to the terminal window.
 	    	System.out.println("Hello, World");
 	    }
 }
