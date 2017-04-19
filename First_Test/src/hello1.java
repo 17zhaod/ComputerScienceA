ec26ad2851badacc1e0e216afc4e32d8ee39c847
daranzhao <daranzhao@darans-mbp>
2017-04-18 20:58:22 -0500

    Uploading some tests

--- First_Test/src/hello1.java
+++ First_Test/src/hello1.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class hello1
 {
   public static void main(String[] args)
@@ -5,5 +7,13 @@ public class hello1
     System.out.println("Hello World!");
     System.out.println("Hello Buddy!");
     System.out.println("Hello please work!");
+    String test = "hello";
+    System.out.println(test.length());
+    String[] test1 = {"wow", "wow", "wow"};
+    System.out.println(test1.length);
+    ArrayList<String> test2 = new ArrayList<String>();
+    test2.add("wow");
+    test2.add("wow");
+    System.out.println(test2.size());
   }
 }
